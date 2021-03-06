c77cd0c9b018c1b16cd06155c1337e14a428148d
jaboulos <jab12288@gmail.com>
2021-03-06 13:27:03 -0800

    Add initial component structure and styling

---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className='font-weight-light display-1 text-center'>
+        Find byte to eat
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
