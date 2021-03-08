3e0cb12ea73d4cafbabc3c2c1ebe9745a7b6ad11
jaboulos <jab12288@gmail.com>
2021-03-07 18:09:25 -0800

    Add cors

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const app = express();
-const PORT = 5000 || process.env.PORT;
+const cors = require('cors');
 
+const app = express();
+// uses cors to allow express to accept requests from other domains
+app.use(cors());
+const PORT = 5000 || process.env.PORT;
 const restaurantsRoute = require('./routes/Restaurants');
 
 // move all routes for restaurant into separate routes file
