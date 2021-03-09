0c38051b71167211d32d90df2f57900c57cce803
jaboulos <jab12288@gmail.com>
2021-03-09 14:50:08 -0800

    Add useEffect hook to update selectedRestaurant context state

--- client/src/pages/RestaurantDetailPage.js
+++ client/src/pages/RestaurantDetailPage.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import { RestaurantsContext } from '../context/RestaurantsContext';
+import { Restaurants } from '../apis';
 
 const RestaurantDetailPage = () => {
-  return <div>Restaurant Detail</div>;
+  const { id } = useParams();
+  const { selectedRestaurant, setSelectedRestaurant } = useContext(
+    RestaurantsContext
+  );
+
+  useEffect(() => {
+    //retrieve data of selected restaurant and store in state variable in context
+    const fetchData = async () => {
+      try {
+        const response = await Restaurants.get(`/${id}`);
+        // update global state to tell rest of app what the selected restaurant is
+        setSelectedRestaurant(response.data.data.restaurants);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+  // add conditional to make sure name is rendered only when the state selectedRestaurant is defined
+  return <div>{selectedRestaurant && selectedRestaurant.name}</div>;
 };
 
 export default RestaurantDetailPage;
